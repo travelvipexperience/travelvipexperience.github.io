76fe9f0a566b575b02c87497ebb646621e11ff1c
Luis Ferreira <luisrbferreira@gmail.com>
2018-08-06 11:28:46 -0300

    Teste

--- js/main.js
+++ js/main.js
@@ -133,21 +133,27 @@ jQuery(document).ready(function($) {
   var $contactForm = $('#contact-form');
   $contactForm.submit(function(e) {
     e.preventDefault();
+    var $submit = $('input:submit', $contactForm);
+    var defaultSubmitText = submit.val();
     $.ajax({
       url: '//formspree.io/xajkqppm',
       method: 'POST',
       data: $(this).serialize(),
       dataType: 'json',
       beforeSend: function() {
-        $contactForm.append('<div class="alert alert--loading">Enviando…</div>');
+        $submit.attr('disabled', true).val('Enviando...');
       },
       success: function(data) {
-        $contactForm.find('.alert--loading').hide();
-        $contactForm.append('<div class="alert alert--success">Dados enviados com sucesso. Entraremos em contato em breve!</div>');
+        $submit.val('Dados enviados com sucesso. Entraremos em contato em breve!');
+        setTimeout(function() {
+          $submit.attr('disabled', false).val(defaultSubmitText);
+        }, 5000);
       },
       error: function(err) {
-        $contactForm.find('.alert--loading').hide();
-        $contactForm.append('<div class="alert alert--error">Ops, algo deu errado, tente enviar novamente!</div>');
+        $submit.val('Ops! Algo deu errado, tente novamente!');
+        setTimeout(function() {
+          $submit.attr('disabled', false).val(defaultSubmitText);
+        }, 5000);
       }
     });
   });
