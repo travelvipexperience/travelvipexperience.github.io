017cf807460a938029b56594fde2f8c0c3add39e
Luis Ferreira <luisrbferreira@gmail.com>
2018-08-15 17:50:10 -0300

    Ajuste

---
+++ js/kleber-sousa.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    $('#celular').mask('(00) 00000-0000');
+    
+    var $contactForm = $('#contact-form');
+    $contactForm.submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+        url: '//formspree.io/mowzoevm',
+        method: 'POST',
+        data: $(this).serialize(),
+        dataType: 'json',
+        beforeSend: function() {
+            $('#erro').hide();
+            $('#enviando').show();
+        },
+        success: function(data) {
+            $('#enviando').hide();
+            $('#info').hide();
+            $('#contact-form').hide();
+            $('#thankYou').show();
+        },
+        error: function(err) {
+            $('#enviando').hide();
+            $('#erro').show();
+        }
+        });
+    });
+});
